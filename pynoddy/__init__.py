c7ff0ba7b0d0ae913c17413a792cf6e04ae48be0
Florian Wellmann <flohorovicic@gmail.com>
2015-10-10 15:52:46 +0200

    Adjusted output in compute_model (only print details in verbose mode)

--- pynoddy/__init__.py
+++ pynoddy/__init__.py
@@ -1,35 +1,36 @@
 """Package initialization file for pynoddy"""
 import os.path
+import subprocess
 
-# Import additional modules of pynoddy
-# from . import history
-#from history import NoddyHistory
-# from . import output
-#from output import NoddyOutput
-#from output import NoddyTopology
-
-#save this module path for relative paths
+# save this module path for relative paths
 package_directory = os.path.dirname(os.path.abspath(__file__))
 
-#paths to noddy & topology executables
-#noddyPath = os.path.join(package_directory,'../noddy/noddy')
-#topologyPath = os.path.join(package_directory,'../topology/topology')
-noddyPath = os.path.join(package_directory,'noddy/noddy')
-topologyPath = os.path.join(package_directory,'topology/topology')
+# paths to noddy & topology executables
+# noddyPath = os.path.join(package_directory,'../noddy/noddy')
+# topologyPath = os.path.join(package_directory,'../topology/topology')
+noddyPath = os.path.join(package_directory, 'noddy/noddy')
+topologyPath = os.path.join(package_directory, 'topology/topology')
 
 # global variables
-ensure_discrete_volumes = True #if True, spatially separated but otherwise identical volumes are given separate codes.
-null_volume_threshold = 20 #volumes smaller than this are ignored completely (as they represent pixelation artefacts).
+ensure_discrete_volumes = True  # if True, spatially separated but otherwise
+# identical volumes are given separate codes.
+null_volume_threshold = 20  # volumes smaller than this are ignored
+# completely (as they represent pixelation artefacts).
 
-#ensure correct noddy & topology builds are present
+# ensure correct noddy & topology builds are present
 if not os.path.exists(noddyPath) and not os.path.exists(noddyPath + ".exe"):
-    print("Error: could not find a compiled version of noddy at %s. Please ensure the source has been compiled (using GCC and compile.bat (windows) or compile.sh (unix))." % noddyPath)
+    print("Error: could not find a compiled version of noddy at %s. \
+    Please ensure the source has been compiled (using GCC and compile.bat \
+    (windows) or compile.sh (unix))." % noddyPath)
 if not os.path.exists(topologyPath) and not os.path.exists(topologyPath + ".exe"):
-    print("Warning: could not find a compiled version of topology at %s. Please ensure the source has been compiled (using GCC and compile.bat (windows) or compile.sh (unix))." % topologyPath)
+    print("Warning: could not find a compiled version of topology at %s. \
+    Please ensure the source has been compiled (using GCC and compile.bat\
+     (windows) or compile.sh (unix))." % topologyPath)
 
-import subprocess
-	
+
 # Some helper functions are defined directly here:
+
+
 def compute_model(history, output_name, **kwds):
     """Call Noddy and compute the history file
     
@@ -38,30 +39,37 @@ def compute_model(history, output_name, **kwds):
         - *output_name* = string : basename for output files
     
     **Optional Keywords**:
-        - *sim_type* = 'BLOCK', 'GEOPHYSICS', 'SURFACES', 'BLOCK_GEOPHYS', 'TOPOLOGY', 'BLOCK_SURFACES', 'ALL':
+        - *sim_type* = 'BLOCK', 'GEOPHYSICS', 'SURFACES', 'BLOCK_GEOPHYS',
+        'TOPOLOGY', 'BLOCK_SURFACES', 'ALL':
             type of Noddy simulation (default: 'BLOCK')
-        - *program_name* = string : name of program (default: noddy.exe or noddy, both checked)
+        - *program_name* = string : name of program
+            (default: noddy.exe or noddy, both checked)
+        - *verbose* = bool: verbose mode, print out information for debugging (default = False)
     **Returns**:
         -Returns any text outputted by the noddy executable.
     """
 
     sim_type = kwds.get("sim_type", 'BLOCK')
-    
-    out = "Running noddy exectuable at %s(.exe)\n" % noddyPath
-    try: #try running .exe file (windows only)
-        out +=  subprocess.Popen([noddyPath+".exe", history, output_name, sim_type], 
-                           shell=False, stderr=subprocess.PIPE, 
-                           stdout=subprocess.PIPE).stdout.read()
-    except OSError: #obviously not running windows - try just the binary
-        out +=  subprocess.Popen([noddyPath, history, output_name, sim_type], 
-                           shell=False, stderr=subprocess.PIPE, 
-                           stdout=subprocess.PIPE).stdout.read()
-                           
-    #Thought: Is there any reason compute_topology should not be called here if sim_type == "TOPOLOGY"???
-    #It could simplify things a lot....
-                           
+
+    if kwds.has_key("verbose") and kwds['verbose']:
+        out = "Running noddy exectuable at %s(.exe)\n" % noddyPath
+    else:
+        out = ""
+    try:  # try running .exe file (windows only)
+        out += subprocess.Popen([noddyPath + ".exe", history, output_name, sim_type],
+                                shell=False, stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE).stdout.read()
+    except OSError:  # obviously not running windows - try just the binary
+        out += subprocess.Popen([noddyPath, history, output_name, sim_type],
+                                shell=False, stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE).stdout.read()
+
+    # Thought: Is there any reason compute_topology should not be called here if sim_type == "TOPOLOGY"???
+    # It could simplify things a lot....
+
     return out
-            
+
+
 def compute_topology(rootname, **kwds):
     """Call the topology executable to compute a models topology.
     
@@ -82,23 +90,23 @@ def compute_topology(rootname, **kwds):
     **Returns**
         -Returns any text outputted by the topology executable, including errors.
     """
-    
-    dvol = kwds.get('ensure_discrete_volumes',ensure_discrete_volumes)
-    nvt =  kwds.get('null_volume_threshold',null_volume_threshold)
-    
-    #convert to string
+
+    dvol = kwds.get('ensure_discrete_volumes', ensure_discrete_volumes)
+    nvt = kwds.get('null_volume_threshold', null_volume_threshold)
+
+    # convert to string
     if dvol:
-        dvol="1"
+        dvol = "1"
     else:
-        dvol="0"
-        
+        dvol = "0"
+
     out = "Running topology exectuable at %s(.exe)\n" % topologyPath
-    try: #try running .exe file (windows only)
-        out =  subprocess.Popen([topologyPath+".exe", rootname, dvol, str(nvt)], 
-                           shell=False, stderr=subprocess.PIPE, 
-                           stdout=subprocess.PIPE).stdout.read()
-    except OSError: #obviously not running windows - try just the binary
-        out =  subprocess.Popen([topologyPath, rootname, dvol, str(nvt)], 
-                           shell=False, stderr=subprocess.PIPE, 
-                           stdout=subprocess.PIPE).stdout.read()
+    try:  # try running .exe file (windows only)
+        out = subprocess.Popen([topologyPath + ".exe", rootname, dvol, str(nvt)],
+                               shell=False, stderr=subprocess.PIPE,
+                               stdout=subprocess.PIPE).stdout.read()
+    except OSError:  # obviously not running windows - try just the binary
+        out = subprocess.Popen([topologyPath, rootname, dvol, str(nvt)],
+                               shell=False, stderr=subprocess.PIPE,
+                               stdout=subprocess.PIPE).stdout.read()
     return out
