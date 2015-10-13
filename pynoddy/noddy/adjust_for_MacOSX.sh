e140af87beb4e81a2743ee3975ec3c90bebc8971
Florian Wellmann <flohorovicic@gmail.com>
2015-10-13 20:08:21 +0200

    Adjusted adjust_for_MacOSX.sh script to actually work with different (strange) encoding settings

--- pynoddy/noddy/adjust_for_MacOSX.sh
+++ pynoddy/noddy/adjust_for_MacOSX.sh
@@ -4,19 +4,19 @@ touch malloc.h
 
 for fl in *.c; do
 mv $fl $fl.old
-LC_CTYPE=C sed 's/<malloc.h>/"malloc.h"/g' $fl.old > $fl
+LC_ALL=C sed 's/<malloc.h>/"malloc.h"/g' $fl.old > $fl
 rm -f $fl.old
 
 done
 for fl in *.h; do
 mv $fl $fl.old
-LC_CTYPE=C sed 's/<malloc.h>/"malloc.h"/g' $fl.old > $fl
+LC_ALL=C sed 's/<malloc.h>/"malloc.h"/g' $fl.old > $fl
 rm -f $fl.old
 done
 
 for fl in dxf*; do
 mv $fl $fl.old
-LC_CTYPE=C sed 's/stricmp/strcmp/g' $fl.old > $fl
+LC_ALL=C sed 's/stricmp/strcmp/g' $fl.old > $fl
 rm -f $fl.old
 done
 
