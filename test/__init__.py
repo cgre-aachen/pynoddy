f3bb10c695d6e20745fe462afb4bb59520cbc2b2
Florian Wellmann <flohorovicic@gmail.com>
2015-10-18 22:28:36 +0200

    Small bug fix in rest

--- test/__init__.py
+++ test/__init__.py
@@ -10,7 +10,7 @@ print package_directory
 
 class TestHistory(TestCase):
     def test_compute_model(self):
-        history = os.path.join(package_directory, "../examples/simple_two_faults.his")
-        output_name = os.path.join(package_directory, "../examples/simple_two_faults_out")
+        history = os.path.join(package_directory, "../test/simple_two_faults.his")
+        output_name = os.path.join(package_directory, "../test/simple_two_faults_out")
         return_val = pynoddy.compute_model(history, output_name)
         assert_equals(return_val, "", msg="Problem with Noddy computation: %s" % return_val)
